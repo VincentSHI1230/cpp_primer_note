4f9f2351a7531cee8e5ba31014c76552ae9b0c31
Vincent1230 <516879257@qq.com>
2023-10-03 17:03:59 +0800

    1-20

--- 1/1-20.cpp
+++ 1/1-20.cpp
@@ -1,8 +1,10 @@
+// 1-20 <inpf >optf
 #include <iostream>
 #include "Sales_item.h"
 int main() {
-    Sales_item a;
-    std::cin >> a;
-    std::cout << a;
+    Sales_item book;
+    while (std::cin >> book) {
+        std::cout << book << std::endl;
+    }
     return 0;
 }
